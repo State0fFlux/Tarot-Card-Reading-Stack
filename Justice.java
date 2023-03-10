c710438c7667942cd044d95b7dd5068a80f32a85
State0fFlux <127552262+State0fFlux@users.noreply.github.com>
2023-03-10 14:27:16 -0800

    Update Justice.java

--- Justice.java
+++ Justice.java
@@ -1,9 +1,8 @@
-// Brady Manske
-// 1/24/23
-// CS&145
-// Deck of Cards: Justice
-
-// This defines a class representing the tarot card, "Justice"
+/*
+ * This defines a class representing the tarot card, "Justice"
+ * @author Brady Manske
+ * @version 2023/01/22
+ */
 public class Justice extends Card {
 
     // Given a boolean representing card orientation,
