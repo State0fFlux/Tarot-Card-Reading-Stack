c24056ace4406f103a2c69b02741e584a08cc455
State0fFlux <127552262+State0fFlux@users.noreply.github.com>
2023-03-10 14:29:54 -0800

    Update TheEmperor.java

--- TheEmperor.java
+++ TheEmperor.java
@@ -1,9 +1,8 @@
-// Brady Manske
-// 1/24/23
-// CS&145
-// Deck of Cards: The Emperor
-
-// This defines a class representing the tarot card, "The Emperor"
+/*
+ * This defines a class representing the tarot card, "The Emperor".
+ * @author Brady Manske
+ * @version 2023/01/22
+ */
 public class TheEmperor extends Card {
 
     // Given a boolean representing card orientation,
