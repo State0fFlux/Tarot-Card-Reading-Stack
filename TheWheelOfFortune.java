23574c7fa460f1fdca639632b3ad810fa8f79af3
State0fFlux <127552262+State0fFlux@users.noreply.github.com>
2023-03-10 14:32:16 -0800

    Update TheWheelOfFortune.java

--- TheWheelOfFortune.java
+++ TheWheelOfFortune.java
@@ -1,9 +1,8 @@
-// Brady Manske
-// 1/24/23
-// CS&145
-// Deck of Cards: The Wheel of Fortune
-
-// This defines a class representing the tarot card, "The Wheel of Fortune"
+/*
+ * This defines a class representing the tarot card, "The Wheel of Fortune".
+ * @author Brady Manske
+ * @version 2023/01/22
+ */
 public class TheWheelOfFortune extends Card {
 
     // Given a boolean representing card orientation,
