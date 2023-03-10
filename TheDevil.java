73d84e28e80c15542245a6bf103e01440f10e2f3
State0fFlux <127552262+State0fFlux@users.noreply.github.com>
2023-03-10 14:29:35 -0800

    Update TheDevil.java

--- TheDevil.java
+++ TheDevil.java
@@ -1,9 +1,8 @@
-// Brady Manske
-// 1/24/23
-// CS&145
-// Deck of Cards: The Devil
-
-// This defines a class representing the tarot card, "The Devil"
+/*
+ * This defines a class representing the tarot card, "The Devil".
+ * @author Brady Manske
+ * @version 2023/01/22
+ */
 public class TheDevil extends Card {
 
     // Given a boolean representing card orientation,
