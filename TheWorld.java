38a4b0a201f79512758334ac1891820cc6636267
State0fFlux <127552262+State0fFlux@users.noreply.github.com>
2023-03-10 14:32:46 -0800

    Update TheWorld.java

--- TheWorld.java
+++ TheWorld.java
@@ -1,9 +1,8 @@
-// Brady Manske
-// 1/24/23
-// CS&145
-// Deck of Cards: The World
-
-// This defines a class representing the tarot card, "The World"
+/*
+ * This defines a class representing the tarot card, "The World".
+ * @author Brady Manske
+ * @version 2023/01/22
+ */
 public class TheWorld extends Card {
 
     // Given a boolean representing card orientation,
