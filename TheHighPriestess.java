15ef39a482c1eb828a85c720a5fafafd29090180
State0fFlux <127552262+State0fFlux@users.noreply.github.com>
2023-03-10 14:31:01 -0800

    Update TheHighPriestess.java

--- TheHighPriestess.java
+++ TheHighPriestess.java
@@ -1,9 +1,8 @@
-// Brady Manske
-// 1/24/23
-// CS&145
-// Deck of Cards: The High Priestess
-
-// This defines a class representing the tarot card, "The High Priestess"
+/*
+ * This defines a class representing the tarot card, "The High Priestess".
+ * @author Brady Manske
+ * @version 2023/01/22
+ */
 public class TheHighPriestess extends Card {
 
     // Given a boolean representing card orientation,
