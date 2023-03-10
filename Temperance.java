73945703c6cb935864e9f22903a75d87d6424c56
State0fFlux <127552262+State0fFlux@users.noreply.github.com>
2023-03-10 14:29:10 -0800

    Update Temperance.java

--- Temperance.java
+++ Temperance.java
@@ -1,9 +1,8 @@
-// Brady Manske
-// 1/24/23
-// CS&145
-// Deck of Cards: Temperance
-
-// This defines a class representing the tarot card, "Temperance"
+/*
+ * This defines a class representing the tarot card, "Temperance".
+ * @author Brady Manske
+ * @version 2023/01/22
+ */
 public class Temperance extends Card {
 
     // Given a boolean representing card orientation,
